97fea551a8d2d0e0001fd2d530d32178668d8e28
kimyej1 <yeji2501@gmail.com>
2022-06-03 10:06:07 +0900

    2022.6.3 10:05

--- A01Hello/app/src/main/java/com/kbstar/android/MainActivity.java
+++ A01Hello/app/src/main/java/com/kbstar/android/MainActivity.java
@@ -3,12 +3,28 @@ package com.kbstar.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);  // R.layout = res > layout 이라는 뜻!
+
+        /*
+            JS
+            var btn = document.getElementById('btn');
+            let btn = document.querySelector('#btn');
+
+            JQuery
+            let btn = $('#btn');
+         */
+
+        textView = findViewById(R.id.textView); // 머 찾아올 땐 find!!!
+        System.out.println("-------------------" + textView.getText().toString());
     }
+
 }
