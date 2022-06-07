e711f9a63480077d213fdebb05f2fff773de69c0
kimyej1 <yeji2501@gmail.com>
2022-06-07 15:34:00 +0900

    2022.6.7 15:33

---
+++ B05Orientation/app/src/main/java/com/kbstar/b05orientation/MainActivity.java
@@ -0,0 +1,89 @@
+package com.kbstar.b05orientation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String name;
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showToast("onCreate()");
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = editText.getText().toString(); // 안드로이드에서는 .toString() 필수!!!!
+
+//                String context = getApplicationContext().toString();
+
+                showToast("Button Clicked. name : " + name);
+            }
+        });
+
+        if(savedInstanceState != null) {
+            name = savedInstanceState.getString("name");                // Cookie 같은 기능
+            showToast(getApplicationContext() + "has been restored.");  // 값 복구
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            showToast("orientation : LANDSCAPE");
+        } else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            showToast("orientation : PORTRAIT");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("name", name);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy()");
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+}
+
+/*
+    AndroidManifest.xml 에 방향 전환 시
+ */
