d59d906b229ed73a39f25dc25bddc605457dfe11
kimyej1 <yeji2501@gmail.com>
2022-06-08 16:04:14 +0900

    2022.6.8 16:04

--- C04CallIntent/app/src/main/java/com/kbstar/c04callintent/MenuActivity.java
+++ C04CallIntent/app/src/main/java/com/kbstar/c04callintent/MenuActivity.java
@@ -26,3 +26,11 @@ public class MenuActivity extends AppCompatActivity {
         });
     }
 }
+
+/*
+    Activity : 화면 하나하나
+    Intent   : activity 사이에 교환하는 데이터 덩어리
+    Bundle   : intent 안에 실제 데이터가 저장되어있는 저장소(hash(dict) = key : value)
+        putExtra()       : Bundle에 값 넣을 때 (rest API(map data) 라서 set 안씀)
+        getStringExtra() : Bundle에서 값 가져올 때
+ */
