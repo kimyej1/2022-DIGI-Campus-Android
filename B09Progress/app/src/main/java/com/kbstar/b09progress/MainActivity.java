89ae2bc2f5e70cbc79a5be13b7a797eeb3462b5e
kimyej1 <yeji2501@gmail.com>
2022-06-08 09:32:30 +0900

    2022.6.8 09:32

--- B09Progress/app/src/main/java/com/kbstar/b09progress/MainActivity.java
+++ B09Progress/app/src/main/java/com/kbstar/b09progress/MainActivity.java
@@ -2,7 +2,9 @@ package com.kbstar.b09progress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar progress;
     Button button;
     Button button2;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,29 @@ public class MainActivity extends AppCompatActivity {
         progress = findViewById(R.id.progressBar);  // 막대형
         progress.setIndeterminate(false);           // false: 멈춤, true: 움직임
         progress.setProgress(70);                   // 70% 진행
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // dialog progress bar
+
+                dialog = new ProgressDialog(MainActivity.this);
+
+                dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("Spinner Test");
+                dialog.show();
+            }
+        });
+    }
+
+    public void kbClick(View view)
+    {
+        if(dialog != null)
+        {
+            dialog.dismiss();
+        }
     }
 }
