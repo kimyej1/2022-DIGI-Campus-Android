77d299d6510d36ae9c58ffed0f7a12203b40ecf5
kimyej1 <yeji2501@gmail.com>
2022-06-10 10:08:19 +0900

    2022.6.10 10:08

--- E02ActionBar/app/src/main/java/com/kbstar/e02actionbar/MainActivity.java
+++ E02ActionBar/app/src/main/java/com/kbstar/e02actionbar/MainActivity.java
@@ -1,4 +1,4 @@
-package com.kbstar.e01optionmenu;
+package com.kbstar.e02actionbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,15 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.kbstar.e02actionbar.R;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    Button button;
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {  // 옵션 안에 아이템이 선택되면 발생하는 이벤
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {  // 옵션 안에 아이템이 선택되면 발생하는 이벤트
 
         // 아이템 한두개면 이렇게 해도 노상관
         if(item.getItemId() == R.id.menu_refresh) {
@@ -55,6 +60,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     public void showToast(String msg) {
