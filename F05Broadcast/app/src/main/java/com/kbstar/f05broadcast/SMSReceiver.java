9fceecaac49225aca3965016773644ab68c0b255
kimyej1 <yeji2501@gmail.com>
2022-06-13 17:26:36 +0900

    2022.6.13 17:26

--- F05Broadcast/app/src/main/java/com/kbstar/f05broadcast/SMSReceiver.java
+++ F05Broadcast/app/src/main/java/com/kbstar/f05broadcast/SMSReceiver.java
@@ -3,6 +3,9 @@ package com.kbstar.f05broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
 
@@ -11,6 +14,16 @@ public class SMSReceiver extends BroadcastReceiver {
     // 문자메시지를 받으면 자동으로 호출
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "-------------------------- onReceive()");
 
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] messages = parseSmsMessage(bundle);
+    }
+
+    private SmsMessage[] parseSmsMessage(Bundle bundle) {
+        Object[] objs = (Object[]) bundle.get("pdus");
+        SmsMessage[] msg = new SmsMessage[objs.length];
+
+        return msg;
     }
 }
