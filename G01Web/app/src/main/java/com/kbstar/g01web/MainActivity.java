8fa5e1b3e0f6089e5c460d18008681fbd28ac933
kimyej1 <yeji2501@gmail.com>
2022-06-15 17:03:45 +0900

    2022.6.15 17:03

--- G01Web/app/src/main/java/com/kbstar/g01web/MainActivity.java
+++ G01Web/app/src/main/java/com/kbstar/g01web/MainActivity.java
@@ -3,7 +3,9 @@ package com.kbstar.g01web;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private Button button;
-    private WebView webView;
+    private WebView webView, popView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +27,26 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
         webView = findViewById(R.id.webView);
+        popView = findViewById(R.id.webView);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+//        webView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+////                return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg);
+//
+//                // 팝업용 웹뷰 속성 설정 (나는 크롬쓰고있는데 엣지 띄워버리면 세션이 끊어지고 정보를 못주고받으니까.. 제어하려고)
+//                popView = new WebView(view.getContext()); // 현재 웹브라우저 가져오기
+//                popView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+//                popView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+//                popView.getSettings().setSupportMultipleWindows(true);
+//
+//                return true;
+//            }
+//        });
+
         webView.setWebViewClient(new ViewClient());
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -42,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     private class ViewClient extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) { // 새 창 띄우지 말고 덮어써라
+        public boolean shouldOverrideUrlLoading(WebView view, String url) { // 새 창 띄지 말고 덮어써라
             view.loadUrl(url);
             return true;
         }
